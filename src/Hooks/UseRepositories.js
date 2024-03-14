809faa74fb4e9daa3c2be333bb4ea2aef8d127b4
Enrique Sabariego <quiquesaba@gmail.com>
2024-03-14 11:45:53 +0100

    update

--- src/Hooks/UseRepositories.js
+++ src/Hooks/UseRepositories.js
@@ -5,7 +5,7 @@ const useRepositories = () => {
   const [repositories, setRepositories] = useState(null)
   
   const fetchRepositories = async () => {
-    const response = await globalThis.fetch('https://c29f-193-43-98-82.ngrok-free.app/api/repositories')
+    const response = await globalThis.fetch('https://c29f-193-43-98-82.ngrok-free.app/api/repositories' || 'http://localhost:5001/api/repositories')
     const json = await response.json()
     setRepositories(json)
   }
